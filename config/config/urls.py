1c5e3193e991bc7a80a12fb356200994f205828d
Tanul Jain <tanuljain25feb@gmail.com>
2021-01-19 00:28:59 +0530

    feat: Add API doc endpoint.

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from to_do_app import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='To Do APP Documentation',
+        default_version='v1',
+        contact=openapi.Contact(email='tanuljain25feb@gmail.com'),
+    ),
+    public=True,
+    )
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/task/', views.LCTaskAPI.as_view()),
-    path('api/task/<int:pk>/', views.RUDTaskAPI.as_view())
+    path('api/v1/task', views.LCTaskAPI.as_view()),
+    path('api/v1/task/<int:pk>', views.RUDTaskAPI.as_view()),
+    url(r'^api/v1/doc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
